f8390a708f69b773f1b7c7a834eeedc3e052f075
pratik12 <p_bidkar@encs.concordia.ca>
2015-02-23 22:17:32 -0500

    cahnges

--- animalGame/src/animalGame/AmphibianJSON.java
+++ animalGame/src/animalGame/AmphibianJSON.java
@@ -1,14 +1,6 @@
 package animalGame;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+public class AmphibianJSON extends ParentJson{
 
-public class AmphibianJSON extends ParentJson {
-	
-	private JSONObject amphJSONQuestionStore = new JSONObject();
-	
-	public AmphibianJSON() throws JSONException{
-		
-	}
+	public AmphibianJSON(){}
 }
--- animalGame/src/animalGame/FishJSON.java
+++ animalGame/src/animalGame/FishJSON.java
@@ -1,13 +1,5 @@
 package animalGame;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectInputStream.GetField;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class FishJSON extends ParentJson{
 	
 	public FishJSON() {
